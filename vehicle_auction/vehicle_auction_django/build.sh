f4d7061fa0916b3d2014d52fd5cfafc1b894e78f
santhanam <363santhanam@gmail.com>
2026-09-09 13:09:14 +0530

    change code

--- vehicle_auction/vehicle_auction_django/build.sh
+++ vehicle_auction/vehicle_auction_django/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Render runs this automatically before starting the app
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --upload-unhashed-files
 python manage.py migrate
